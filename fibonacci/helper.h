fa4bd717c5e5cc92d84d1fe3fa6733cd8b9c6681
RP-Lens <85287880+RP-Lens@users.noreply.github.com>
2021-11-27 16:41:13 +0000

    Update helper.h

--- fibonacci/helper.h
+++ fibonacci/helper.h
@@ -9,10 +9,12 @@
 //Function checks for valid argv
 int checkNum(char *s);
 //Function finds and prints the fibonacci sequence up to N
-void fibonacci(long long N, FILE *out);
+void fibonacci(unsigned long long N, FILE *out);
 //Function that finds current date and converts it to string
 char* getDate();
 //Function that watermarks outfile
 void watermark(FILE *out);
+//Function that finds current time and returns strformat
+char* getTime(void);
 
 #endif  //End of helper.h declaration
